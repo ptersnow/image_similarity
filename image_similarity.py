98080885e234426c9e7f96853cdfe5681d75bac7
Pedro Henrique Neves da Silva <ptersnow@gmail.com>
2023-11-09 17:05:57 -0400

    fix: added cosine similarity.

--- image_similarity.py
+++ image_similarity.py
@@ -15,10 +15,10 @@ model = SentenceTransformer('clip-ViT-L-14')
 print("="*80)
 print()
 
-novos_pronacs = ['1112166', '1311023']
+novos_pronacs = ['C']
 
 working_directory = os.path.dirname(os.path.realpath(__file__))
-images_dir = os.path.join(working_directory, "pronacs")
+images_dir = os.path.join(working_directory, "pronacs_teste")
 results_dir = os.path.join(working_directory, "analise")
 
 if not os.path.exists(results_dir):
@@ -35,21 +35,19 @@ def find_duplicates_and_similar_images(pronac, image_paths1, image_paths2):
     duplicates = []
     threshold = 0.75
 
-    encoded_images1 = model.encode([Image.open(filepath) for filepath in image_paths1], batch_size=128, convert_to_tensor=True, show_progress_bar=True)
-    encoded_images2 = model.encode([Image.open(filepath) for filepath in image_paths2], batch_size=128, convert_to_tensor=True, show_progress_bar=True)
+    encoded_images1 = model.encode([Image.open(filepath) for filepath in image_paths1], batch_size=128, convert_to_tensor=True)
+    encoded_images2 = model.encode([Image.open(filepath) for filepath in image_paths2], batch_size=128, convert_to_tensor=True)
 
-    processed_images1 = util.paraphrase_mining_embeddings(encoded_images1)
-    processed_images2 = util.paraphrase_mining_embeddings(encoded_images2)
+    results = util.cos_sim(encoded_images1, encoded_images2)
 
-    for score, image_id1, image_id2 in processed_images1:
-        for image2 in processed_images2:
-            score2, _, _ = image2
-            if (score2 >= threshold) and (image_id2 < len(image_paths2)):
+    for i in range(len(encoded_images1)):
+        for j in range(len(encoded_images2)):
+            if (results[i, j] >= threshold):
                 duplicates.append({
                                   "pronac": pronac,
-                                  "image1": image_paths1[image_id1],
-                                  "image2": image_paths2[image_id2],
-                                  "score": score
+                                  "image1": image_paths1[i],
+                                  "image2": image_paths2[j],
+                                  "score": results[i, j].item()
                                   })
     return duplicates
 
@@ -66,16 +64,17 @@ if __name__ == "__main__":
 
         if len(image_paths1) > 0:
 
-            print("Comparando com novos pronacs")
-            for j in range(i + 1, len(novos_pronacs)):
-                image_paths2 = load_images_from_directory(novos_pronacs[j])
-        
-                print("-"*80)
-                print(f"Encontrado {len(image_paths2)} imagens em {novos_pronacs[j]}")
+            if len(novos_pronacs) > 1:
+                print("Comparando com novos pronacs")
+                for j in range(i + 1, len(novos_pronacs)):
+                    image_paths2 = load_images_from_directory(novos_pronacs[j])
+            
+                    print("-"*80)
+                    print(f"Encontrado {len(image_paths2)} imagens em {novos_pronacs[j]}")
 
-                if len(image_paths2) > 0:
-                    print(f'Buscando duplicatas entre {novo_pronac} e {novos_pronacs[j]}...')
-                    data.extend(find_duplicates_and_similar_images(novos_pronacs[j], image_paths1, image_paths2))
+                    if len(image_paths2) > 0:
+                        print(f'Buscando duplicatas entre {novo_pronac} e {novos_pronacs[j]}...')
+                        data.extend(find_duplicates_and_similar_images(novos_pronacs[j], image_paths1, image_paths2))
 
             print("Comparando com pronacs antigos")
             with os.scandir(images_dir) as it:
@@ -91,10 +90,9 @@ if __name__ == "__main__":
                             data.extend(find_duplicates_and_similar_images(entry.name, image_paths1, image_paths2))
 
             print("-"*80)
-            print(f"Salvando arquivo {out_file}")
+            print(f"Salvando arquivo {out_file}... ", end="")
             with open(out_file, 'w') as fp:
                 json.dump(data, fp, indent=4)
-
-            print()
+            print("[DONE]")
         else:
             print("Nada a fazer")
